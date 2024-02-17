49fa8c57ba3ec44b5d87d55f50c993fbb88f95a2
ga-patrick-kittle <patrick.kittle@gmail.com>
2024-02-17 11:51:10 +0800

    created working mine map creator function and refacxtored some variables/consts

--- app.js
+++ app.js
@@ -1,50 +1,74 @@
-//TODO Setup the game constants
-let inPlay = false;
-let isWinner = false;
-let score = 0;
-let timer = 0;
-let totalMines = 0;
-mapWidth = 5;
-mapHeight = 5;
-var mapX = mapWidth;
+/* constants */
+
+/* state variables (initialise in function */
+let inPlay;
+let isWinner;
+let score;
+let timer;
+const totalMines = 10; //this will also be set at game setup - here for testing
+const mapWidth = 5; //this  and height will be set by thr user in game setup in final
+const mapHeight = 5;
+const mapArray = [];
+var mapX = mapWidth; // use for itteration
 var mapY = mapHeight;
+const minesArray = [{x: 1, y: 1}]; //testing 1,1 values
 
+/* cached DOM elements (such as the baord innerHTML perhaps) */
 
+/* event listeners */
 
+/* functions */
 
-const createMapArray = (mapArray) => {
+// TODO setup database connection and object for high scores
+
+const createMapArray = mapArray => {
   //base case - both x and y have gone to 0
-  if ( mapX === 0 ) {
+  if (mapX === 0) {
     console.log(`Hit the base case: mapX is: ${mapX}`);
     mapArray.pop();
     return mapArray;
   }
-
-  if (mapY > 0 ) {
-    console.log(`In the standard row loop and mapY is: ${mapY} and mapX is ${mapX}`)
+  //recursion cases
+  if (mapY > 0) {
+    console.log(
+      `In the standard row loop and mapY is: ${mapY} and mapX is ${mapX}`
+    );
     newCellObj = {x: mapX, y: mapY, mine: false, cleared: false, adjacent: 0};
     mapY -= 1;
-    return createMapArray([...mapArray, newCellObj])
+    return createMapArray([...mapArray, newCellObj]);
   }
-  if (mapY === 0  && mapX !== 0) {
+  if (mapY === 0 && mapX !== 0) {
     mapY = mapWidth;
     mapX -= 1;
     console.log(`In the NEW row loop and mapY is: ${mapY} and mapX is ${mapX}`);
     newCellObj = {x: mapX, y: mapY, mine: false, cleared: false, adjacent: 0};
     mapY -= 1;
-    return createMapArray([...mapArray, newCellObj])
+    return createMapArray([...mapArray, newCellObj]);
   }
-}
+};
 
+const createMineMapCoords = minesArray => {
+  const mineCount = minesArray.length ?? 0; // checking for the  initial case to we don't break on startup
+  // Base case
+  if (minesArray.length === totalMines) return minesArray;
 
-
-
-
-
-
-
-const createMineMapCoords = () => {
-  // TODO create random locations for the mines
+  // recursion case
+  const x = Math.floor(Math.random() * mapWidth);
+  const y = Math.floor(Math.random() * mapHeight);
+  console.log(`x: ${x} and y: ${y} height: ${mapHeight}`);
+  const newCoord = {x: x, y: y};
+  // logic to check for duplicates
+  const preExisting = minesArray.some(coord => {
+    if (coord.x === x && coord.y === y) {
+      return true;
+    }
+    return false;
+  });
+  console.log(preExisting);
+  return preExisting
+    ? createMineMapCoords([...minesArray])
+    : createMineMapCoords([...minesArray, newCoord]);
+  //return createMineMapCoords([...minesArray, newCoord]);
 };
 
 const initialise = () => {
@@ -54,8 +78,9 @@ const initialise = () => {
 //TODO add event listeners
 
 const render = () => {
-  // TODO
+  // TODO add multiple render functions
+  // renderBoard()
+  // renderControls etc etc
 };
 
-
-//Test Calls
+//Testing Calls

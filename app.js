d4deaa30fd8fd6d495e0ac77b1fcf0551698c498
ga-patrick-kittle <patrick.kittle@gmail.com>
2024-02-18 13:02:42 +0800

    create the create rows function
    creating x row div elements to popultae with the cells

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /* constants */
-
+const gameBoardContainer = document.querySelector('.game-board-container');
+const playButton = document.querySelector('.btn-play');
+// console.log(gameBoardContainer);
 /* state variables (initialise in function */
 let inPlay;
 let isWinner;
@@ -8,42 +10,37 @@ let timer;
 const totalMines = 10; //this will also be set at game setup - here for testing
 const mapWidth = 5; //this  and height will be set by thr user in game setup in final
 const mapHeight = 5;
-const mapArray = [];
+//const mapArray = [];  need to check the scope of this as we may need to declare globally if i need it outside of initialise
 var mapX = mapWidth; // use for itteration
 var mapY = mapHeight;
-const minesArray = [{x: 1, y: 1}]; //testing 1,1 values
+// const minesArray = []; //testing 1,1 values
 
 /* cached DOM elements (such as the baord innerHTML perhaps) */
-
-/* event listeners */
 
 /* functions */
 
 // TODO setup database connection and object for high scores
 
-const createMapArray = mapArray => {
+const createMapArray = mapArr => {
   //base case - both x and y have gone to 0
   if (mapX === 0) {
-    console.log(`Hit the base case: mapX is: ${mapX}`);
-    mapArray.pop();
-    return mapArray;
+    //console.log(`Hit the base case: mapX is: ${mapX}`);
+    mapArr.pop();
+    return mapArr;
   }
   //recursion cases
   if (mapY > 0) {
-    console.log(
-      `In the standard row loop and mapY is: ${mapY} and mapX is ${mapX}`
-    );
     newCellObj = {x: mapX, y: mapY, mine: false, cleared: false, adjacent: 0};
     mapY -= 1;
-    return createMapArray([...mapArray, newCellObj]);
+    return createMapArray([...mapArr, newCellObj]);
   }
   if (mapY === 0 && mapX !== 0) {
     mapY = mapWidth;
     mapX -= 1;
-    console.log(`In the NEW row loop and mapY is: ${mapY} and mapX is ${mapX}`);
+    //console.log(`In the NEW row loop and mapY is: ${mapY} and mapX is ${mapX}`);
     newCellObj = {x: mapX, y: mapY, mine: false, cleared: false, adjacent: 0};
     mapY -= 1;
-    return createMapArray([...mapArray, newCellObj]);
+    return createMapArray([...mapArr, newCellObj]);
   }
 };
 
@@ -51,11 +48,10 @@ const createMineMapCoords = minesArray => {
   const mineCount = minesArray.length ?? 0; // checking for the  initial case to we don't break on startup
   // Base case
   if (minesArray.length === totalMines) return minesArray;
-
   // recursion case
-  const x = Math.floor(Math.random() * mapWidth);
-  const y = Math.floor(Math.random() * mapHeight);
-  console.log(`x: ${x} and y: ${y} height: ${mapHeight}`);
+  const x = Math.floor(Math.random() * mapWidth + 1);
+  const y = Math.floor(Math.random() * mapHeight + 1);
+  //console.log(`x: ${x} and y: ${y} height: ${mapHeight}`);
   const newCoord = {x: x, y: y};
   // logic to check for duplicates
   const preExisting = minesArray.some(coord => {
@@ -64,23 +60,53 @@ const createMineMapCoords = minesArray => {
     }
     return false;
   });
-  console.log(preExisting);
+  //console.log(preExisting); // check if the exact element already exists - if it does, return without adding
   return preExisting
     ? createMineMapCoords([...minesArray])
     : createMineMapCoords([...minesArray, newCoord]);
   //return createMineMapCoords([...minesArray, newCoord]);
 };
 
-const initialise = () => {
+const initialiseGame = () => {
   // TODO setup the game
+  mapArray = createMapArray([]);
+  minesArray = createMineMapCoords([]);
+  renderInitialBoard(mapArray);
 };
 
-//TODO add event listeners
+/* event listeners */
 
-const render = () => {
-  // TODO add multiple render functions
-  // renderBoard()
-  // renderControls etc etc
+playButton.addEventListener('click', initialiseGame);
+
+gameBoardContainer.addEventListener('click', () => {
+  console.log('Game board container was clicked');
+});
+
+const renderRowElements = numRows => {
+  for (let i = 1; i <= numRows; i++) {
+    gameBoardContainer.innerHTML += `<div class="row row-${i}"></div>`;
+  }
 };
 
-//Testing Calls
+const renderInitialBoard = arr => {
+  const NEW_ROW_START = `<div class="row">`;
+  const NEW_ROW_END = `</div>`;
+  const boardCells = [];
+  let i = arr.length - 1;
+  for (i; i >= 0; i--) {
+    const cell = arr[i];
+    if (cell.y === 1) {
+      gameBoardContainer.innerHTML += NEW_ROW_START;
+      boardCells.push(NEW_ROW_START);
+    }
+    newCell = `<div class="col-sm" data-id="${cell.x}-${cell.y}">CL</div>`;
+    gameBoardContainer.innerHTML += newCell;
+    // cache the cell to an array we can return
+    boardCells.push(newCell);
+    if (cell.y === 5) {
+      gameBoardContainer.innerHTML += NEW_ROW_END;
+      boardCells.push(NEW_ROW_END);
+    }
+  }
+  return boardCells;
+};

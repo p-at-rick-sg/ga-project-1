0903592ee45996dbcff5e03127c3854d72460821
ga-patrick-kittle <patrick.kittle@gmail.com>
2024-02-22 13:50:55 +0800

    recursion appears to be working OK for the whole flood mechanism

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const createMapArray = width => {
   }
   for (row in mapArray) {
     for (cell in mapArray) {
-      mapArray[row][cell] = {flagged: false, checked: 0, value: null};
+      mapArray[row][cell] = {flagged: false, checked: false, value: null};
     }
   }
   return mapArray;
@@ -106,108 +106,47 @@ const initialiseGame = e => {
   addMinesToMap(mapArray, minesArray);
   renderRowElements(mapHeight);
   renderColumnElements(mapArray);
-  // addNeighborsToMap();
-  // console.log(mapArray);
 };
 
-/* making space to work on the recurive element only */
+const tmpZeroCells = []; //testing with this outside
 
-const checkMap = cellArr => {
-  perimeterCellsArr = perimeterCells(cellArr);
-  const neighborsMineCnt = checkPerimeterMines(perimeterCellsArr, cellArr);
-  if (neighborsMineCnt === 0) {
-    console.log('the checked cell is so we continue after setting the cell to 0 and checked');
-    for (cell of perimeterCellsArr) {
-      checkPerimeterMines(perimeterCellsArr, cell);
-    }
+const floodRecursion = cellArr => {
+  console.log('the flooding recursive function start line');
+  // BASE CASE - ALL NEIGHBOURS ARE CHECKED (and maybe we end up back at the start?)
+
+  const perimeterCellsArr = checkForPerimeterCells(cellArr);
+  console.log('unchecked vaid perimeter cells: ' + perimeterCellsArr);
+  for (cell of perimeterCellsArr) {
   }
+  //check each unchecked perimeter cell for value
+
+  perimeterCellsArr.forEach(cell => {
+    [x, y] = cell;
+    //we get the valid perimeter cells for the perimeter cell
+    const tmpPerimeterCellsArr = checkForPerimeterCells(cell);
+    // get the value for that cell
+    const tmpCellValue = checkPerimeterMineValue(tmpPerimeterCellsArr, cellArr);
+    //update stage and UI
+    if ([1, 2, 3, 4, 5, 6, 7, 8, 9].includes(tmpCellValue)) {
+      updateCellState(x, y, tmpCellValue);
+      updateCellUI(x, y, tmpCellValue);
+      //we don't add to the next to check array as we have a value and we don't check value cells
+    } else if (tmpCellValue === 0) {
+      // set the cell state and UI then call the recursive function
+      updateCellState(x, y, tmpCellValue);
+      updateCellUI(x, y, tmpCellValue);
+      //add this cellArr to the tmpZeroCells array
+      tmpZeroCells.push(cell);
+    }
+  });
+  //now I need to call the recursion again with a value from the tmpZeroCells array
+  const nextCell = tmpZeroCells.pop();
+  floodRecursion(nextCell);
 };
 
 /* END OF RECURSIVE ELEMENT WORK  */
 
-const checkPerimeterMines = (cellsArr, currentCell) => {
-  let neighborMineCount = 0;
-  console.log('cellsArr in the checkpermiter func' + cellsArr);
-  for (cell of cellsArr) {
-    console.log();
-    if (mapArray[cell[0]][cell[1]].value === 'mine') {
-      neighborMineCount++;
-      console.log(`cell: ${cell} was logged as a mine`);
-    }
-  }
-  mapArray[currentCell[0]][currentCell[1]].value = neighborMineCount;
-  updateCell(currentCell[0], currentCell[1], neighborMineCount);
-  return neighborMineCount;
-};
-
-const updateCell = (x, y, option) => {
-  cellToUpdate = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
-  const imgElement = cellToUpdate.children[0];
-
-  switch (option) {
-    case 1:
-      imgElement.src = '/img/cell-1.png';
-      break;
-    case 2:
-      imgElement.src = '/img/cell-2.png';
-      break;
-    case 3:
-      imgElement.src = '/img/cell-3.png';
-      break;
-    case 4:
-      imgElement.src = '/img/cell-4.png';
-      break;
-    case 5:
-      imgElement.src = '/img/cell-5.png';
-      break;
-    case 6:
-      imgElement.src = '/img/cell-6.png';
-      break;
-    case 'mine':
-      imgElement.src = '/img/mine-cell.png';
-      break;
-    default:
-      imgElement.src = '/img/cleared-cell.png';
-  }
-};
-
-const handleBoardRightClick = e => {
-  e.preventDefault();
-  const x = e.target.parentElement.getAttribute('data-x');
-  const y = e.target.parentElement.getAttribute('data-y');
-  console.log(e.target.getAttribute('src'));
-  if (e.target.getAttribute('src') === '/img/flag-cell.png') {
-    mapArray[x][y].flagged = false;
-    e.target.src = '/img/blank-cell.png';
-  } else {
-    mapArray[x][y].flagged = true;
-    e.target.src = '/img/flag-cell.png';
-  }
-};
-
-const handleBoardLeftClick = e => {
-  e.preventDefault();
-  const x = e.target.parentElement.getAttribute('data-x');
-  const y = e.target.parentElement.getAttribute('data-y');
-  //console.log(`x: ${x} y:${y}`);
-  selectedCell = mapArray[x][y];
-  //do initial check for mine in this left clicked cell
-  if (selectedCell.value === 'mine') {
-    //call the board update function to show all mines
-    for (cell of minesArray) {
-      updateCell(cell.x, cell.y, 'mine');
-    }
-    isWinner = false;
-    gameOver = true; // not sure I need this - work out later how to stop the game
-  } else {
-    // here we trigger the checkMap to check the map recursively - update to call with a 2 element array for x, y
-    const cellArr = [x, y];
-    checkMap(cellArr);
-  }
-  //checkCells(mapArray[selectedCellIdx]); // LATER
-};
-
-const perimeterCells = cellArr => {
+const checkForPerimeterCells = cellArr => {
   //checking for all valid cells (not OOB) we need mines as we need to count them
   const x = Number(cellArr[0]);
   const y = Number(cellArr[1]);
@@ -223,14 +162,154 @@ const perimeterCells = cellArr => {
   ];
   const filteredPerimeterCells = [];
   for (arr of permiterCellsArr) {
-    if (arr[0] < 0 || arr[1] < 0 || arr[0] > mapWidth - 1 || arr[1] > mapHeight - 1) {
-      //console.log(`Cell is out of bounds ${arr}`);
+    if (
+      arr[0] < 0 ||
+      arr[1] < 0 ||
+      arr[0] > mapWidth - 1 ||
+      arr[1] > mapHeight - 1 ||
+      mapArray[arr[0]][arr[1]].checked === true
+    ) {
       continue;
     } else {
       filteredPerimeterCells.push(arr);
     }
   }
   return filteredPerimeterCells;
+};
+
+const checkPerimeterMineValue = (cellsArr, currentCell) => {
+  let neighborMineCount = 0;
+  for (cell of cellsArr) {
+    if (mapArray[cell[0]][cell[1]].value === 'mine') {
+      neighborMineCount++;
+      console.log(`cell: ${cell} was logged as a mine`);
+    }
+  }
+  return neighborMineCount;
+};
+
+const updateCellUI = (x, y, option) => {
+  console.log('updating UI');
+  cellToUpdate = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+  const imgElement = cellToUpdate.children[0];
+  switch (option) {
+    case 1:
+      imgElement.src = '/img/cell-1.png';
+      mapArray[x][y].checked = true;
+      break;
+    case 2:
+      imgElement.src = '/img/cell-2.png';
+      mapArray[x][y].checked = true;
+      break;
+    case 3:
+      imgElement.src = '/img/cell-3.png';
+      mapArray[x][y].checked = true;
+      break;
+    case 4:
+      imgElement.src = '/img/cell-4.png';
+      mapArray[x][y].checked = true;
+      break;
+    case 5:
+      imgElement.src = '/img/cell-5.png';
+      mapArray[x][y].checked = true;
+      break;
+    case 6:
+      imgElement.src = '/img/cell-6.png';
+      mapArray[x][y].checked = true;
+      break;
+    case 'mine':
+      imgElement.src = '/img/mine-cell.png';
+      mapArray[x][y].checked = true;
+      break;
+    default:
+      imgElement.src = '/img/cleared-cell.png';
+      mapArray[x][y].checked = true;
+  }
+};
+
+const updateCellState = (x, y, option) => {
+  console.log('updaing state');
+  cellToUpdate = mapArray[x][y];
+  switch (option) {
+    case 1:
+      cellToUpdate.value = 1;
+      cellToUpdate.checked = true;
+      break;
+    case 2:
+      cellToUpdate.value = 2;
+      cellToUpdate.checked = true;
+      break;
+    case 3:
+      cellToUpdate.value = 3;
+      cellToUpdate.checked = true;
+      break;
+    case 4:
+      cellToUpdate.value = 4;
+      cellToUpdate.checked = true;
+      break;
+    case 5:
+      cellToUpdate.value = 5;
+      cellToUpdate.checked = true;
+      break;
+    case 6:
+      cellToUpdate.value = 6;
+      cellToUpdate.checked = true;
+      break;
+    case 'mine':
+      cellToUpdate.value = 'mine';
+      cellToUpdate.checked = true;
+      break;
+    default:
+      cellToUpdate.value = 0;
+      cellToUpdate.checked = true;
+  }
+};
+
+const handleBoardRightClick = e => {
+  e.preventDefault();
+  const x = e.target.parentElement.getAttribute('data-x');
+  const y = e.target.parentElement.getAttribute('data-y');
+  if (e.target.getAttribute('src') === '/img/flag-cell.png') {
+    mapArray[x][y].flagged = false;
+    e.target.src = '/img/blank-cell.png';
+  } else {
+    mapArray[x][y].flagged = true;
+    e.target.src = '/img/flag-cell.png';
+  }
+};
+
+const handleBoardLeftClick = e => {
+  e.preventDefault();
+  const x = e.target.parentElement.getAttribute('data-x');
+  const y = e.target.parentElement.getAttribute('data-y');
+  const selectedCell = mapArray[x][y];
+  //do initial check for mine in this left clicked cell
+  if (selectedCell.value === 'mine') {
+    //call the board update function to show all mines
+    for (cell of minesArray) {
+      updateCellState(cell.x, cell.y, 'mine');
+      updateCellUI(cell.x, cell.y, 'mine');
+    }
+    isWinner = false;
+    gameOver = true; // not sure I need this - work out later how to stop the game
+    console.log('the game should break after this and display the notification div');
+    return 0;
+  }
+  // check if the cell is a number and then return to the game
+  cellArr = [x, y];
+  perimeterCellsArr = checkForPerimeterCells(cellArr);
+  const cellValue = checkPerimeterMineValue(perimeterCellsArr, cellArr);
+  //if the value is in the range 1-9 then reveal the cell and exit
+  if ([1, 2, 3, 4, 5, 6, 7, 8, 9].includes(cellValue)) {
+    updateCellState(x, y, cellValue);
+    updateCellUI(x, y, cellValue);
+    return 0;
+  } else if (cellValue === 0) {
+    // set the cell state and UI then call the recursive function
+    updateCellState(x, y, cellValue);
+    updateCellUI(x, y, cellValue);
+    floodRecursion(cellArr);
+  }
 };
 
 const renderRowElements = numRows => {
@@ -245,7 +324,6 @@ const renderColumnElements = cellArr => {
   //loop the rows and add the child cell elements
   for (row in allRowDivs) {
     for (cell in cellArr[row]) {
-      //console.log(`row idx: ${row} and cell idx: ${cell}`);
       allRowDivs[
         row
       ].innerHTML += `<button type="button" class="col btn btn-cell" data-x="${row}" data-y="${cell}" ><img src="/img/blank-cell.png" /></button>`;
